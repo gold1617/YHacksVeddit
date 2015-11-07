f0c51a2de8b3cbc4a69e8380b96ca0ae98bd1462
Rishab Chhabra <rishab.chhabra@stonybrook.edu>
2015-11-07 08:15:43 -0500

    Removed fab, added android default settings menu

--- Veddit/app/src/main/java/me/yhack/veddit/ScrollingActivity.java
+++ Veddit/app/src/main/java/me/yhack/veddit/ScrollingActivity.java
@@ -1,5 +1,6 @@
 package me.yhack.veddit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,14 +19,16 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+     /*   FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+
         });
+        */
     }
 
     @Override
@@ -44,6 +47,11 @@ public class ScrollingActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settings = new Intent(this, SettingsActivity.class);
+            startActivity(settings);
+
+        //    Snackbar.make(this.findViewById(android.R.id.content), "Replace with your own action", Snackbar.LENGTH_LONG)
+          //          .setAction("Action", null).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
